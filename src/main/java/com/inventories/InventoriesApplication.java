71dfc639809a768f77bbde68d067a7e4e10307c2
Stevano <stevanocandra@gmail.com>
2018-10-24 15:54:28 +0700

    remove sentry.io

--- src/main/java/com/inventories/InventoriesApplication.java
+++ src/main/java/com/inventories/InventoriesApplication.java
@@ -14,12 +14,7 @@ import javax.servlet.Filter;
 //@ComponentScan(basePackages = "com.inventories")
 public class InventoriesApplication {
 
-    private static SentryClient sentry;
-
     public static void main(String[] args) {
-        Sentry.init("https://9ffe2965c3034768a1c6e6524d2f1c70@sentry.io/1254226");
-        sentry = SentryClientFactory.sentryClient();
-
         SpringApplication.run(InventoriesApplication.class, args);
     }
 
